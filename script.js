0a31767c262eac4d0fd6d30f1f2752eb12a8865b
chenar-farhad <chenar.farhad@gmail.com>
2021-11-02 19:36:51 +0300

    1st upload

---
+++ script.js
@@ -0,0 +1,52 @@
+window.onload = function(){
+ AOS.init();
+}
+
+function cvDownload() {
+  window.open(
+    "https://drive.google.com/file/d/1GgMRy-8l4D_UugEdA9FYiTLO2W8gAEnX/view?usp=sharing",
+    "_blank"
+  );
+}
+function facebook() {
+  window.open("https://www.facebook.com/chenar.farhad", "_blank");
+}
+function twitter() {
+  window.open("https://www.twitter.com/chenar_farhad", "_blank");
+}
+function instagram() {
+  window.open("https://www.instagram.com/chenar_farhad", "_blank");
+}
+function github() {
+  window.open("https://github.com/chenar-farhad", "_blank");
+}
+function linkedin() {
+  window.open("https://www.linkedin.com/in/chenar-farhad", "_blank");
+}
+
+/* LAKATI SCROLL NAV BAR RANGI AGORE */
+
+// const navv = document.querySelector('.navv');
+// window.onscroll=function(){
+//     var top = window.scrollY;
+//     console.log(top);
+//     if(top>=50){
+//         navv.classList.add('active');}
+//         else{
+//             navv.classList.remove('active');
+//         }
+//     }
+
+/*  RANG GORIN */
+function color1() {
+  document.documentElement.setAttribute("style", "--color1: blueviolet");
+}
+function color2() {
+  document.documentElement.setAttribute("style", "--color1: #ebd323");
+}
+function color3() {
+  document.documentElement.setAttribute("style", "--color1: #4C60ED");
+}
+function color4() {
+  document.documentElement.setAttribute("style", "--color1: #d45757");
+}
